0de0fbb5b0d73e19a8aab628361a0ed9af33dc60
Belenista <113619267+belenista@users.noreply.github.com>
2025-05-26 13:07:23 -0300

    Add files via upload

---
+++ Torneos/Persona.cpp
@@ -0,0 +1,23 @@
+///Ejercicio:
+///Autor:Bel
+///Fecha:
+///Comentario:
+
+# include<iostream>
+
+using namespace std;
+# include "Persona.h"
+
+Persona::Persona(){
+    _dni="";
+    _nombre="";
+    _apellido="";
+    _tel="";
+    _eMail="";
+    fechaNacimiento=0
+}
+ Persona::Persona(char _dni, char nombre, char _apellido, char _tel, char _email, char fechaNacimiento){
+
+
+ }
+
